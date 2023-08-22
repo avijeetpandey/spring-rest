356cde6fce9ef7e4c388b9e7062333396b76b769
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 12:27:37 +0530

    Add: empty controller

--- src/main/java/com/example/springrest/controller/CourseController.java
+++ src/main/java/com/example/springrest/controller/CourseController.java
@@ -1,14 +1,18 @@
 package com.example.springrest.controller;
 
+import com.example.springrest.entity.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CourseController {
 
-    // mapping for getting home page
-    @GetMapping("/home")
-    public String home() {
-        return "";
+    // mapping for getting all courses
+    @GetMapping("/courses")
+    public List<Course> getCourses() {
+        return new ArrayList<>();
     }
 }
