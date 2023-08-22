90b6e44f55a6439cb925b21efc458b8904f1188d
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 12:32:54 +0530

    Add: course service and ingterface

---
+++ src/main/java/com/example/springrest/services/CourseService.java
@@ -0,0 +1,9 @@
+package com.example.springrest.services;
+
+import com.example.springrest.entity.Course;
+
+import java.util.List;
+
+public interface CourseService {
+    public List<Course> getCourses();
+}
---
+++ src/main/java/com/example/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.springrest.services;
+
+import com.example.springrest.entity.Course;
+
+import java.util.List;
+
+public class CourseServiceImpl implements CourseService{
+    @Override
+    public List<Course> getCourses() {
+        return null;
+    }
+}
+
