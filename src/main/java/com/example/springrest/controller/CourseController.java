6c4f689d58209e87e4813c1f5a2d3d73b15f8b8d
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 15:09:33 +0530

    Add: Get courses controller and services

--- src/main/java/com/example/springrest/controller/CourseController.java
+++ src/main/java/com/example/springrest/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.example.springrest.controller;
 
 import com.example.springrest.entity.Course;
+import com.example.springrest.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,12 @@ import java.util.List;
 
 @RestController
 public class CourseController {
+    @Autowired
+    private CourseService service;
 
     // mapping for getting all courses
     @GetMapping("/courses")
     public List<Course> getCourses() {
-        return new ArrayList<>();
+        return this.service.getCourses();
     }
 }
--- src/main/java/com/example/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/example/springrest/services/CourseServiceImpl.java
@@ -1,13 +1,27 @@
 package com.example.springrest.services;
 
 import com.example.springrest.entity.Course;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CourseServiceImpl implements CourseService{
+
+    List<Course> list;
+
+    // default constructor
+    public CourseServiceImpl() {
+        list = new ArrayList<>();
+        list.add(new Course(123,"Title one","description one"));
+        list.add(new Course(124,"Title two","description two"));
+        list.add(new Course(125,"Title three","description three"));
+    }
+
     @Override
     public List<Course> getCourses() {
-        return null;
+        return this.list;
     }
 }
 
